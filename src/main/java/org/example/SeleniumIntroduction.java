8cbe25a8d67c809557daf9ef1eaaad8227eff3b3
Abdullah Al Faroque <rfnshare@gmail.com>
2024-06-22 00:21:28 +0600

    added tesng into maven

---
+++ src/main/java/org/example/SeleniumIntroduction.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class SeleniumIntroduction {
+//    public static void main(String[] args) {
+//        WebDriver driver = new ChromeDriver();
+//        driver.manage().window().maximize();
+//        driver.get("https://qa.cats.csafeglobal.com/");
+//        System.out.println(driver.getTitle());
+//        System.out.println(driver.getCurrentUrl());
+//        driver.close();
+//        driver.quit();
+//    }
+    @Test
+    public void Demo()
+    {
+        System.out.println("Selenium introduction");
+    }
+    @Test
+    public void DemoTwo()
+    {
+        System.out.println("Selenium introduction Two");
+    }
+}
