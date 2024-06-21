0fc740b5074c7b95bb13d5614910703cd43fd063
Abdullah Al Faroque <rfnshare@gmail.com>
2024-06-22 00:22:19 +0600

    added testng hooks

--- src/main/java/org/example/SeleniumIntroduction.java
+++ src/main/java/org/example/SeleniumIntroduction.java
@@ -1,8 +1,6 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.Test;
+package org.example;
+
+import org.testng.annotations.*;
 
 public class SeleniumIntroduction {
 //    public static void main(String[] args) {
@@ -23,5 +21,26 @@ public class SeleniumIntroduction {
     public void DemoTwo()
     {
         System.out.println("Selenium introduction Two");
+    }
+    @BeforeTest
+    public void prerequisite()
+    {
+        System.out.println("Selenium introduction Initiate");
+    }
+
+    @AfterTest
+    public void teardown()
+    {
+        System.out.println("Selenium introduction End");
+    }
+    @BeforeSuite
+    public void bs()
+    {
+        System.out.println("Before Suite");
+    }
+    @AfterSuite
+    public void as()
+    {
+        System.out.println("After Suite");
     }
 }
