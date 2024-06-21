c00fe5d03cc151968439d70227e5ecbc9db985a5
Abdullah Al Faroque <afaroque@relisource.com>
2024-06-22 00:31:05 +0600

    into to selenium

--- src/main/java/org/example/SeleniumIntroduction.java
+++ src/main/java/org/example/SeleniumIntroduction.java
@@ -1,46 +1,48 @@
 package org.example;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
 
 public class SeleniumIntroduction {
-//    public static void main(String[] args) {
-//        WebDriver driver = new ChromeDriver();
-//        driver.manage().window().maximize();
-//        driver.get("https://qa.cats.csafeglobal.com/");
-//        System.out.println(driver.getTitle());
-//        System.out.println(driver.getCurrentUrl());
-//        driver.close();
-//        driver.quit();
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://qa.cats.csafeglobal.com/");
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        driver.close();
+        driver.quit();
+    }
+//    @Test
+//    public void Demo()
+//    {
+//        System.out.println("Selenium introduction");
 //    }
-    @Test
-    public void Demo()
-    {
-        System.out.println("Selenium introduction");
-    }
-    @Test
-    public void DemoTwo()
-    {
-        System.out.println("Selenium introduction Two");
-    }
-    @BeforeTest
-    public void prerequisite()
-    {
-        System.out.println("Selenium introduction Initiate");
-    }
-
-    @AfterTest
-    public void teardown()
-    {
-        System.out.println("Selenium introduction End");
-    }
-    @BeforeSuite
-    public void bs()
-    {
-        System.out.println("Before Suite");
-    }
-    @AfterSuite
-    public void as()
-    {
-        System.out.println("After Suite");
-    }
+//    @Test
+//    public void DemoTwo()
+//    {
+//        System.out.println("Selenium introduction Two");
+//    }
+//    @BeforeTest
+//    public void prerequisite()
+//    {
+//        System.out.println("Selenium introduction Initiate");
+//    }
+//
+//    @AfterTest
+//    public void teardown()
+//    {
+//        System.out.println("Selenium introduction End");
+//    }
+//    @BeforeSuite
+//    public void bs()
+//    {
+//        System.out.println("Before Suite");
+//    }
+//    @AfterSuite
+//    public void as()
+//    {
+//        System.out.println("After Suite");
+//    }
 }
