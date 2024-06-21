375dbdc445dbfb109864043cdc267d4253613d4d
Abdullah Al Faroque <afaroque@relisource.com>
2024-06-22 00:29:43 +0600

    variable concepts

---
+++ src/main/java/org/example/CoreJavaBrushUp1.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class CoreJavaBrushUp1 {
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println(num);
+        String name = "Sample Text";
+        System.out.println(name);
+
+        char letter = 'A';
+        System.out.println(letter);
+
+        float number = 3.14f;
+        System.out.println(number);
+
+        boolean bool = true;
+        System.out.println(bool);
+
+    }
+}
