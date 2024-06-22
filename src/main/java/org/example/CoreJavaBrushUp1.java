4e7975375e2100e7b3e745489ee6e22381d6bc46
Abdullah Al Faroque <rfnshare@gmail.com>
2024-06-22 11:25:11 +0600

    array concepts

--- src/main/java/org/example/CoreJavaBrushUp1.java
+++ src/main/java/org/example/CoreJavaBrushUp1.java
@@ -16,5 +16,25 @@ public class CoreJavaBrushUp1 {
         boolean bool = true;
         System.out.println(bool);
 
+        // Array
+
+        int[] array = new int[5];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        array[3] = 4;
+        array[4] = 5;
+        System.out.println(array[3]);
+
+        int[] array2 = {1, 2, 3, 4, 5,6,7,8,9};
+        for (int i = 0; i < array2.length; i++) {
+            System.out.println(array2[i]);
+        }
+
+        String[] names = {"Sample Text", "Sample Text", "Sample Text", "Sample Text"};
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+
     }
 }
