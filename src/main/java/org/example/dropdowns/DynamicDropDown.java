239233d3adf9d4bf7949e41ed13e098e65b440f5
Abdullah Al Faroque <rfnshare@gmail.com>
2024-06-23 00:32:57 +0600

    handaling dynamic window dropdown

---
+++ src/main/java/org/example/dropdowns/DynamicDropDown.java
@@ -0,0 +1,19 @@
+package org.example.dropdowns;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DynamicDropDown {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+        // //div[contains(@id, 'destinationStation')]/parent::div//a[@value='MAA']
+        driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//div[contains(@id, 'originStation')]/parent::div//a[@value='BLR']")).click();
+//        driver.findElement(By.id("ctl00_mainContent_ddl_destinationStation1_CTXT")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//div[contains(@id, 'destinationStation')]/parent::div//a[@value='MAA']")).click();
+    }
+}
