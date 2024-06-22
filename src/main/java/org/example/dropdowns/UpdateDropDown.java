c90fa08228201cde71a82f96127cb011bcfd64e8
Abdullah Al Faroque <rfnshare@gmail.com>
2024-06-22 23:53:45 +0600

    click dropdown & useing loop

---
+++ src/main/java/org/example/dropdowns/UpdateDropDown.java
@@ -0,0 +1,31 @@
+package org.example.dropdowns;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+
+public class UpdateDropDown {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+        driver.findElement(By.id("divpaxinfo")).click();
+        Thread.sleep(1000);
+        WebElement adult = driver.findElement(By.id("hrefIncAdt"));
+//        for (int i = 0; i < 4; i++) {
+//            adult.click();
+//        }
+        int i=0;
+        while(i<4)
+        {
+            adult.click();
+            i++;
+        }
+        driver.findElement(By.id("btnclosepaxoption")).click();
+        Assert.assertEquals(driver.findElement(By.id("divpaxinfo")).getText(), "5 Adult");
+        Thread.sleep(2000);
+        driver.quit();
+    }
+}
