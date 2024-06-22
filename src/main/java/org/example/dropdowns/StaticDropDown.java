0ba01ae6119ccc5c53faab9b643de63c803fff73
Abdullah Al Faroque <rfnshare@gmail.com>
2024-06-22 23:37:31 +0600

    practicing static dropdown using selenium

--- src/main/java/org/example/dropdowns/StaticDropDown.java
+++ src/main/java/org/example/dropdowns/StaticDropDown.java
@@ -1,5 +1,23 @@
 package org.example.dropdowns;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 public class StaticDropDown {
-
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+        WebElement element = driver.findElement(By.cssSelector("select[id*='DropDownListCurrency']"));
+        Select select = new Select(element);
+        select.selectByIndex(3);
+        System.out.println(select.getFirstSelectedOption().getText());
+        select.selectByVisibleText("INR");
+        System.out.println(select.getFirstSelectedOption().getText());
+        select.selectByValue("AED");
+        System.out.println(select.getFirstSelectedOption().getText());
+        Thread.sleep(2000);
+        driver.quit();
+    }
 }
