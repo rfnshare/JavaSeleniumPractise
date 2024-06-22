f753297fadb86c5735d07b502d63e66110bbb400
Abdullah Al Faroque <rfnshare@gmail.com>
2024-06-22 12:11:13 +0600

    play with dynamic array

--- src/main/java/org/example/CoreJavaBrushUp2.java
+++ src/main/java/org/example/CoreJavaBrushUp2.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class CoreJavaBrushUp2 {
     public static void main(String[] args) {
         int[] array2 = {1,2,3,4,5,6,7,8,9, 10, 122};
@@ -27,5 +29,18 @@ public class CoreJavaBrushUp2 {
             sum+=numbers[i-1];
         }
         System.out.println(sum);
+
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("Hello");
+        list.add("World");
+        list.add("Java");
+        System.out.println(list);
+        list.remove(1);
+        System.out.println(list.get(1));
+        for(String s : list)
+        {
+            System.out.println(s);
+        }
+
     }
 }
