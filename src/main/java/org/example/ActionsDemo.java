05a6e44ee631c95999c6f2a54e91d4ace28f609c
Abdullah Al Faroque <rfnshare@gmail.com>
2024-06-24 10:19:24 +0600

    exploring actions with hovering, keydown, right click etc

--- src/main/java/org/example/ActionsDemo.java
+++ src/main/java/org/example/ActionsDemo.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,8 +11,12 @@ public class ActionsDemo {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.amazon.com/");
+        driver.manage().window().maximize();
         WebElement account_hover = driver.findElement(By.xpath("//a[@id='nav-link-accountList']"));
         Actions actions = new Actions(driver);
-        actions.moveToElement(account_hover).build().perform();
+        WebElement search_box = driver.findElement(By.id("twotabsearchtextbox"));
+//        actions.moveToElement(search_box).click().keyDown(Keys.SHIFT).sendKeys("hello").doubleClick().build().perform();
+//        actions.moveToElement(account_hover).build().perform();
+        actions.moveToElement(account_hover).contextClick().build().perform();
     }
 }
