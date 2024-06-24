0fd540ab42cd492ddd6edff6bc050f366ce110d5
Abdullah Al Faroque <rfnshare@gmail.com>
2024-06-24 19:52:16 +0600

    handaling & validating multiple links

--- src/main/java/org/example/Scope.java
+++ src/main/java/org/example/Scope.java
@@ -1,11 +1,15 @@
 package org.example;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.security.Key;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 public class Scope {
     public static void main(String[] args) {
@@ -19,5 +23,15 @@ public class Scope {
 
         WebElement ul = footer.findElement(By.xpath("//table/tbody/tr/td[1]/ul"));
         System.out.println(ul.findElements(By.tagName("a")).size());
+        for (int i = 1; i < ul.findElements(By.tagName("a")).size(); i++) {
+            String clickolinktab = Keys.chord(Keys.CONTROL, Keys.ENTER);
+            ul.findElements(By.tagName("a")).get(i).sendKeys(clickolinktab);
+        }
+        Set<String> windows = driver.getWindowHandles();
+        Iterator<String> iterator= windows.iterator();
+        while (iterator.hasNext()) {
+            driver.switchTo().window(iterator.next());
+            System.out.println(driver.getTitle());
+        }
     }
 }
