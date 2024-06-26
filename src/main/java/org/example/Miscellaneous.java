b14b5e007fca586af872a4affe747e79a65d3706
Abdullah Al Faroque <rfnshare@gmail.com>
2024-06-26 13:14:40 +0600

    impliment take ss

--- src/main/java/org/example/Miscellaneous.java
+++ src/main/java/org/example/Miscellaneous.java
@@ -1,11 +1,17 @@
 package org.example;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Miscellaneous {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ChromeOptions options = new ChromeOptions();
         options.setAcceptInsecureCerts(true);
         WebDriver driver = new ChromeDriver(options);
@@ -15,6 +21,8 @@ public class Miscellaneous {
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
         driver.manage().deleteCookieNamed("a");
+        File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(src, new File("E:\\Screenshot\\screenshot.png"));
 
         driver.quit();
     }
