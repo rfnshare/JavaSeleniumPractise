4ee02952e229c3d9611acf6af2ab56afbb900041
Abdullah Al Faroque <rfnshare@gmail.com>
2024-06-28 21:09:23 +0600

    validating component ui through getRect

--- src/main/java/org/example/InvokingMultipleWindows.java
+++ src/main/java/org/example/InvokingMultipleWindows.java
@@ -3,6 +3,7 @@ package org.example;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.io.File;
 import java.io.IOException;
@@ -11,7 +12,9 @@ import java.util.Set;
 import java.util.Iterator;
 public class InvokingMultipleWindows {
     public static void main(String[] args) throws IOException {
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions option = new ChromeOptions();
+        option.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(option);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.manage().window().maximize();
         driver.get("https://rahulshettyacademy.com/angularpractice/");
@@ -28,6 +31,9 @@ public class InvokingMultipleWindows {
         nameEditBox.sendKeys(courseName);
         File file = nameEditBox.getScreenshotAs(OutputType.FILE);
         FileUtils.copyFile(file, new File("E:\\Screenshot\\screenshot.png"));
+
+        System.out.println("Height: "+nameEditBox.getRect().getHeight());;
+        System.out.println("Width: "+nameEditBox.getRect().getWidth());;
         driver.quit();
     }
 }
